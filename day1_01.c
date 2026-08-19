a53315153053091b8dc33a9f919d4ed849fc72eb
Aarush Girdhar <Aarush.29875@stu.upes.ac.in>
2026-08-19 19:20:10 +0530

    Day 1: C programming

---
+++ day1_01.c
@@ -0,0 +1,13 @@
+// Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+
+int main(void)
+{
+	int firstNumber, secondNumber;
+
+	scanf("%d %d", &firstNumber, &secondNumber);
+	printf("Sum = %d\n", firstNumber + secondNumber);
+
+	return 0;
+}
