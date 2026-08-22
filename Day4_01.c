05cd52e0fdbfe8f1e3b6a339cdbd5a0116509c5d
Aarush Girdhar <Aarush.29875@stu.upes.ac.in>
2026-08-22 22:01:49 +0530

    Day 4 : C programming

---
+++ Day4_01.c
@@ -0,0 +1,18 @@
+// Swap two numbers without using a third variable.
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	printf("Enter two numbers: ");
+	scanf("%d %d", &a, &b);
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	printf("After swapping: a = %d, b = %d\n", a, b);
+
+	return 0;
+}
