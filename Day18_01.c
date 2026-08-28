d08ac130b3b2a4b96778c2d9cc844ab0a12e26b0
Aarush Girdhar <Aarush.29875@stu.upes.ac.in>
2026-08-28 08:31:47 +0530

    Day 18: C programming

---
+++ Day18_01.c
@@ -0,0 +1,21 @@
+// Write a program to print all factors of a given number.
+#include <stdio.h>
+
+int main()
+{
+	int number;
+
+	printf("Enter a positive number: ");
+	scanf("%d", &number);
+
+	printf("Factors of %d are: ", number);
+	for (int i = 1; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return 0;
+}
