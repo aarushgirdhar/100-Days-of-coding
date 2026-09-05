95e6df20dc37a947df5a60f5f6f6613995a35f43
Aarush Girdhar <Aarush.29875@stu.upes.ac.in>
2026-09-05 22:34:25 +0530

    Day 24: Q2

--- Day24_02.c
+++ Day24_02.c
@@ -1,6 +1,23 @@
- #include <stdio.h>
+/* Q48: Write a program to print the following pattern:
+1
+12
+123
+1234
+12345
 
-int main(void)
+Sample Test Cases:
+Input 1:
+
+Output 1:
+1
+12
+123
+1234
+12345
+*/
+#include <stdio.h>
+
+int main()
 {
 	for (int i = 1; i <= 5; i++) {
 		for (int j = 1; j <= i; j++) {
